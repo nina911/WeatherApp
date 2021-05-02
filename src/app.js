0ead220352f3a7c7b2d36cd41f67e82bc29a6c04
Nina Schüller <Nina.Schueller@amana.de>
2021-05-02 12:22:39 +0200

    added description for current weather

--- src/app.js
+++ src/app.js
@@ -51,12 +51,13 @@ function displayWeatherCondition(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
+
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
   document.querySelector("#high").innerHTML = Math.round(
     response.data.main.temp_max
   );
