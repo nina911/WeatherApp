3117db125f18dbdba5cc61907c9266e920995384
nina911 <31767157+nina911@users.noreply.github.com>
2021-05-08 14:53:32 +0200

    api integration forecast

--- src/app.js
+++ src/app.js
@@ -44,30 +44,47 @@ function formatTime(time) {
   return `${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
+  //let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2">
-          <div class="forecast-date">${day}</div>
+          <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
           <img
-            src="http://openweathermap.org/img/wn/50d@2x.png"
+            src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }.png"
             alt=""
             width="42"
           />
           <div class="forecast-temperatures">
-            <span class="forecast-temperature-max"> 🔺18° </span>
-            <span class="forecast-temperature-min"> 🔻12° </span>
+            <span class="forecast-temperature-max"> 🔺${Math.round(
+              forecastDay.temp.max
+            )}° </span>
+            <span class="forecast-temperature-min"> 🔻${Math.round(
+              forecastDay.temp.min
+            )}° </span>
           </div>
         </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
